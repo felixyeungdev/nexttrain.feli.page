0d126fb4a2d5cb5832ed7eba54dd44d35b203c32
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-07-05 19:22:25 +0800

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: "media",
+    mode: "jit",
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
